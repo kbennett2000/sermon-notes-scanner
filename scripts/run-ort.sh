f0814071f635d4a600a19dabdc5361540d51ecc2
egdels <christian.kierdorf@schliweb.de>
2026-03-22 09:05:58 +0100

    Disable SBT package manager in ORT configuration and update `run-ort.sh` accordingly

--- scripts/run-ort.sh
+++ scripts/run-ort.sh
@@ -12,8 +12,7 @@ echo "==> Running ORT analyze"
 ort --config "${ROOT_DIR}/ort/config/config.yml" \
   analyze \
   -i "${ROOT_DIR}" \
-  -o "${OUT_DIR}/analyzer" \
-  --package-managers -SBT
+  -o "${OUT_DIR}/analyzer"
 
 echo "==> Running ORT scan"
 ort --config "${ROOT_DIR}/ort/config/config.yml" \
